ceb3d797316dd40e8bfe1f4ed01353efba9dae11
cuixiaorui <cui_xiaorui@126.com>
2020-05-31 17:03:35 +0800

    review(play-plane): change stage width and height

--- example/play-plane/game.js
+++ example/play-plane/game.js
@@ -3,7 +3,10 @@ import * as PIXI from "PIXI.js";
 export const game = initPixi();
 
 function initPixi() {
-  let app = new PIXI.Application({ width: 500, height: 500 });
+  let app = new PIXI.Application({
+    width: 750,
+    height: 800,
+  });
   app.renderer.backgroundColor = 0x061639;
   document.body.appendChild(app.view);
   return app;
