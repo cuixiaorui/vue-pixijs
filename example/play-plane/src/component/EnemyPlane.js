0ad4b6d79144f5fde21891e984c315d33401c523
cuixiaorui <cui_xiaorui@126.com>
2020-06-02 14:25:02 +0800

    feat(play-plane): add enemyplane attact feature

--- example/play-plane/src/component/EnemyPlane.js
+++ example/play-plane/src/component/EnemyPlane.js
@@ -1,11 +1,41 @@
 import { game } from "../../game";
 import { useKeyboardMove, useKeyboard } from "../use";
-import { h, defineComponent, ref } from "../../../../src/index";
+import {
+  h,
+  defineComponent,
+  ref,
+  watch,
+  onMounted,
+  onUnmounted,
+} from "../../../../src/index";
 // 敌方飞机
 export default defineComponent({
+  props: ["x", "y"],
   setup(props, ctx) {
-    const x = ref(150);
-    const y = ref(0);
+    const x = ref(props.x);
+    const y = ref(props.y);
+
+    watch(props, (newValue) => {
+      x.value = newValue.x;
+      y.value = newValue.y;
+    });
+
+    // 发射子弹
+    const attackInterval = 2000;
+    let intervalId;
+    onMounted(() => {
+      intervalId = setInterval(() => {
+        ctx.emit("attack", {
+          x: x.value + 105,
+          y: y.value + 200,
+        });
+      }, attackInterval);
+    });
+
+    onUnmounted(() => {
+      clearInterval(intervalId);
+    });
+
     return {
       x,
       y,
--- example/play-plane/src/component/GameContainer.js
+++ example/play-plane/src/component/GameContainer.js
@@ -3,71 +3,91 @@ import Plane from "./Plane.js";
 import Map from "./Map.js";
 import EnemyPlane from "./EnemyPlane";
 import { game } from "../../game";
+import { hitTestRectangle } from "../utils";
 import { h, reactive, ref } from "../../../../src/index.js";
+import { moveBullets } from "../moveBullets";
+import { moveEnemyPlane } from "../moveEnemyPlane";
+import { stage } from "../config/index.js";
 
 let hashCode = 0;
+const createHashCode = () => {
+  return hashCode++;
+};
+
 export default {
   setup() {
-    const selfBullets = reactive([]);
-    const enemyPlanes = reactive([
-      {
-        x: 150,
-        y: 0,
+    //生产敌机
+    const createEnemyPlaneData = (x) => {
+      return {
+        x,
+        y: -200,
         width: 217,
         height: 263,
-      },
-    ]);
-
-    const createHashCode = () => {
-      return hashCode++;
+        life: 3,
+      };
     };
+    const selfBullets = reactive([]);
+    const enemyPlanes = reactive([]);
+    const enemyPlaneBullets = reactive([]);
+
+    setInterval(() => {
+      const x = Math.floor((1 + stage.width) * Math.random());
+      enemyPlanes.push(createEnemyPlaneData(x));
+    }, 1000);
 
     const handleBulletDestroy = ({ id }) => {
       const index = selfBullets.findIndex((info) => info.id == id);
-      selfBullets.splice(index, 1);
+      if (index !== -1) {
+        selfBullets.splice(index, 1);
+      }
     };
 
     const handlePlaneAttack = ({ x, y }) => {
       const id = createHashCode();
       const width = 26;
       const height = 37;
-      selfBullets.push({ x, y, id, width, height });
+      const dir = -1;
+      selfBullets.push({ x, y, id, width, height, dir });
     };
 
-    // todo
-    // 检测子弹的位置是否和敌军的位置相交
-    // 如果相交的话，就认定为碰撞了
-    // 销毁子弹  销毁碰撞到的敌军
-    // 在销毁之前 可以添加爆炸效果到敌军的位置
-    // 公式
-    function boxesIntersect(ab, bb) {
-      return (
-        ab.x + ab.width > bb.x &&
-        ab.x < bb.x + bb.width &&
-        ab.y + ab.height > bb.y &&
-        ab.y < bb.y + bb.height
-      );
-    }
-
-    const bulletSpeed = 3;
-    const vanishLine = -100;
-    const isDisappear = (val) => val < vanishLine;
+    const handleEnemyPlaneAttack = ({ x, y }) => {
+      const id = createHashCode();
+      const width = 26;
+      const height = 37;
+      const dir = 1;
+      enemyPlaneBullets.push({ x, y, id, width, height, dir });
+    };
 
     game.ticker.add(() => {
-      selfBullets.forEach((bullet, index) => {
-        bullet.y -= bulletSpeed;
-        if (isDisappear(bullet.y)) {
-          selfBullets.splice(index, 1);
-        }
-      });
+      moveBullets(selfBullets);
+      moveBullets(enemyPlaneBullets);
+      moveEnemyPlane(enemyPlanes);
 
       // 先遍历所有的子弹
-      selfBullets.forEach((bullet, index) => {
-        enemyPlanes.forEach((enemyPlane) => {
-          const isIntersect = boxesIntersect(bullet, enemyPlane);
+      selfBullets.forEach((bullet, selfIndex) => {
+        // 检测我方子弹是否碰到了敌机
+        enemyPlanes.forEach((enemyPlane, enemyPlaneIndex) => {
+          const isIntersect = hitTestRectangle(bullet, enemyPlane);
           if (isIntersect) {
-            console.log("碰上啦");
-            selfBullets.splice(index, 1);
+            selfBullets.splice(selfIndex, 1);
+
+            // 敌机需要减血
+            enemyPlane.life--;
+            if (enemyPlane.life <= 0) {
+              // todo
+              // 可以让实例发消息过来在销毁
+              // 因为需要在销毁之前播放销毁动画
+              enemyPlanes.splice(enemyPlaneIndex, 1);
+            }
+          }
+        });
+
+        // 检测是否碰到了敌方子弹
+        enemyPlaneBullets.forEach((enemyBullet, enemyBulletIndex) => {
+          const isIntersect = hitTestRectangle(bullet, enemyBullet);
+          if (isIntersect) {
+            selfBullets.splice(selfIndex, 1);
+            enemyPlaneBullets.splice(enemyBulletIndex, 1);
           }
         });
       });
@@ -76,13 +96,15 @@ export default {
     return {
       enemyPlanes,
       selfBullets,
+      enemyPlaneBullets,
       handleBulletDestroy,
       handlePlaneAttack,
+      handleEnemyPlaneAttack,
     };
   },
 
   render(ctx) {
-    const createBullet = (info) => {
+    const createBullet = (info, index) => {
       return h(Bullet, {
         key: "Bullet" + info.id,
         x: info.x,
@@ -101,23 +123,18 @@ export default {
         y: info.y,
         height: info.height,
         width: info.width,
+        onAttack: ctx.handleEnemyPlaneAttack,
       });
     };
 
-    return h(
-      "Container",
-      {
-        width: 500,
-        height: 500,
-      },
-      [
-        h(Map),
-        h(Plane, {
-          onAttack: ctx.handlePlaneAttack,
-        }),
-        ...ctx.selfBullets.map(createBullet),
-        ...ctx.enemyPlanes.map(createEnemyPlane),
-      ]
-    );
+    return h("Container", [
+      h(Map),
+      h(Plane, {
+        onAttack: ctx.handlePlaneAttack,
+      }),
+      ...ctx.selfBullets.map(createBullet),
+      ...ctx.enemyPlaneBullets.map(createBullet),
+      ...ctx.enemyPlanes.map(createEnemyPlane),
+    ]);
   },
 };
